39768e61c2aa528439d03eb5ec769baa9676894e
40407502 <tcregan01@qub.ac.>
2024-07-26 22:51:02 +0100

    Updated app.js for deployment

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const http = require('http');
+const path = require('path'); // Import path module
 const socketIo = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes'); // Ensure this path is correct
-const User = require('./models/user'); // Import the User model
+const userRoutes = require('./routes/userRoutes');
+const User = require('./models/user');
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['http://localhost:3001', 'http://192.168.1.100:3001'], // Allow both localhost and your IP
+    origin: '*', // For development only. Restrict this in production.
     methods: ['GET', 'POST'],
   },
 });
 
 // Middleware
 app.use(cors({
-  origin: '*', // For development only. Restrict this in production
+  origin: '*', // For development only. Restrict this in production.
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,6 +34,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 // Use user routes
 app.use('/api', userRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 // Socket.IO connection
 io.on('connection', (socket) => {
@@ -55,6 +63,6 @@ io.on('connection', (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
