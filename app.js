e373ba7c981c2d0c2e38aefd419d0112aefe8d35
40407502 <tcregan01@qub.ac.>
2024-07-27 14:20:31 +0100

    use * for origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['https://beer-tracker-backend.onrender.com'], // Allow both localhost and your IP
+    origin: ['*'], // Allow both localhost and your IP
     methods: ['GET', 'POST'],
   },
 });
